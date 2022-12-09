fb6d7e71493413ebfe1a36148b2aa9595dcf4f36
marek.hirschner <marek.hirschner@muziker.com>
2022-12-09 07:16:04 +0100

    Day 9 first part

---
+++ day_9/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+struct knot {
+    int pos_x = 0, pos_y = 0;
+};
+
+void move_tail(const knot &head, knot &tail){
+    if(head.pos_y == tail.pos_y && head.pos_x == tail.pos_x)
+        return;
+    if(head.pos_y != tail.pos_y && head.pos_x != tail.pos_x){
+        if(abs(abs(head.pos_y) - abs(tail.pos_y)) == 1 && abs(abs(head.pos_x) - abs(tail.pos_x)) == 1)
+            return;
+
+        if(head.pos_y > tail.pos_y && head.pos_x > tail.pos_x){
+            tail.pos_y++;
+            tail.pos_x++;
+        }
+        else if(head.pos_y > tail.pos_y && head.pos_x < tail.pos_x){
+            tail.pos_y++;
+            tail.pos_x--;
+        }
+        if(head.pos_y < tail.pos_y && head.pos_x > tail.pos_x){
+            tail.pos_y--;
+            tail.pos_x++;
+        }
+        else if(head.pos_y < tail.pos_y && head.pos_x < tail.pos_x){
+            tail.pos_y--;
+            tail.pos_x--;
+        }
+    }
+
+    if(abs(abs(head.pos_y) - abs(tail.pos_y)) == 1 && head.pos_x == tail.pos_x ||
+        abs(abs(head.pos_x) - abs(tail.pos_x)) == 1 && head.pos_y == tail.pos_y)
+        return;
+    else if(head.pos_y > tail.pos_y){
+        tail.pos_y++;
+    }
+    else if(head.pos_y < tail.pos_y){
+        tail.pos_y--;
+    }
+    else if(head.pos_x > tail.pos_x){
+        tail.pos_x++;
+    }
+    else if(head.pos_x < tail.pos_x){
+        tail.pos_x--;
+    }
+}
+
+int main() {
+    string line, direction, amount;
+    ifstream input("../day_9/input.txt");
+    stringstream ss;
+
+    set<string> visited;
+    knot head, tail;
+
+    if (input.is_open()) {
+        while (getline(input, line)) {
+            ss.str(line);
+            getline(ss, direction, ' ');
+            getline(ss, amount, ' ');
+            for(int i = 0; i < stoi(amount); i++){
+                switch(direction[0]){
+                    case 'R':
+                        head.pos_x++;
+                        break;
+                    case 'D':
+                        head.pos_y--;
+                        break;
+                    case 'L':
+                        head.pos_x--;
+                        break;
+                    case 'U':
+                        head.pos_y++;
+                        break;
+                }
+                move_tail(head, tail);
+                visited.insert(to_string(tail.pos_x) + ':' + to_string(tail.pos_y));
+            }
+            ss.clear();
+        }
+        input.close();
+    } else {
+        cout << "Unable to open file";
+        return -1;
+    }
+
+    cout << "Result: " << visited.size() << endl;
+    return 0;
+}
