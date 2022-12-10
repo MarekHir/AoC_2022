2b45f52c8d08ca7728074268be843ca165f31560
marek.hirschner <marek.hirschner@muziker.com>
2022-12-10 16:17:14 +0100

    Day 10 second part

--- day_10/main.cpp
+++ day_10/main.cpp
@@ -6,28 +6,34 @@
 
 using namespace std;
 
-void check_cycle(int &strength, int &cycle, int &x){
-    if((cycle == 20 || (cycle - 20) % 40 == 0) && cycle <= 220){
-        strength += cycle * x;
+void check_cycle(int &cycle, int &x){
+//    cout << cycle << " " << x ;
+    if(x + 1 >= cycle % 40 && cycle % 40 >= x - 1){
+        cout << '#';
+    }else{
+        cout << '.';
+    }
+    if((cycle + 1) % 40 == 0){
+        cout << endl;
     }
 }
 
-void noop(int &strength, int &cycle, int &x){
+void noop(int &cycle, int &x){
+    check_cycle( cycle, x);
     cycle++;
-    check_cycle(strength, cycle, x);
 }
 
-void addx(int amount, int &strength, int &cycle, int &x){
+void addx(int amount,  int &cycle, int &x){
     for(int i = 0; i < 2; i++){
+        check_cycle(cycle, x);
         cycle++;
-        check_cycle(strength, cycle, x);
     }
     x += amount;
 }
 
 int main() {
     string line, line_part;
-    int strength_total = 0, cycle_count = 0, x_register = 1;
+    int cycle_count = 0, x_register = 1;
     ifstream input("../day_10/input.txt");
     stringstream s;
 
@@ -36,10 +42,10 @@ int main() {
             s.str(line);
             getline(s,line_part, ' ');
             if(line_part == "noop"){
-                noop(strength_total, cycle_count, x_register);
+                noop(cycle_count, x_register);
             }else if(line_part == "addx"){
                 getline(s,line_part);
-                addx(stoi(line_part), strength_total, cycle_count, x_register);
+                addx(stoi(line_part), cycle_count, x_register);
             }
             s.clear();
         }
@@ -48,8 +54,6 @@ int main() {
         cout << "Unable to open file";
         return -1;
     }
-
-    cout << "Result: " << strength_total << endl;
 
     return 0;
 }
